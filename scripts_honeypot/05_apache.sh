a383bbfa036b6b5c63b3f31477e09fd8bbec9bef
ernestoo-v <evillarperez@gmail.com>
2025-05-31 15:03:32 +0200

    apache creation fixed

--- scripts_honeypot/05_apache.sh
+++ scripts_honeypot/05_apache.sh
@@ -5,12 +5,7 @@ set -euo pipefail
 # 1) Calculo de la ruta absoluta al propio directorio de este script:
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# 3) Identificar explícitamente el setup de Apache
-APACHE_SETUP="$SCRIPTS_DIR/create_apache.sh"
-
-echo "==> Iniciando generación de páginas web desde $SCRIPTS_DIR …"
-
-# 4) Primero: creamos Dockerfile y estructura Apache/PHP
+# 2) Primero: creamos Dockerfile y estructura Apache/PHP
 if [[ -f "$APACHE_SETUP" ]]; then
   echo "----> Ejecutando $(basename "$APACHE_SETUP")"
   bash "$APACHE_SETUP"
@@ -19,7 +14,7 @@ else
   exit 1
 fi
 
-# 5) Copiamos el directorio de nuestra web al directorio de honeypot
+# 3) Copiamos el directorio de nuestra web al directorio de honeypot
 sudo cp -r web/* honeypot/web
 
 echo "==> ¡Todas las páginas web se han generado correctamente!"
