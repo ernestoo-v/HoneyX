5445e6c6ac4b3853337386e6a5bda18c2ba545b7
ernestoo-v <evillarperez@gmail.com>
2025-06-14 18:20:51 +0200

    promtail restarting fixed

--- scripts_honeypot/01_setup.sh
+++ scripts_honeypot/01_setup.sh
@@ -5,15 +5,6 @@ set -e
 BASE_DIR="honeypot"
 
 echo "==> Creando directorios en $BASE_DIR..."
-mkdir -p \ 
-  "$BASE_DIR/ftp" \ 
-  "$BASE_DIR/web" \ 
-  "$BASE_DIR/volumenes/apache_logs" \ 
-  "$BASE_DIR/volumenes/mysql_log" \ 
-  "$BASE_DIR/volumenes/ftp_logs" \ 
-  "$BASE_DIR/volumenes/fakessh_logs" \ 
-  "$BASE_DIR/volumenes/mysql_data" \ 
-  "$BASE_DIR/volumenes/config" \ 
-  "$BASE_DIR/config"
+mkdir -p "$BASE_DIR/ftp" "$BASE_DIR/web" "$BASE_DIR/volumenes/apache_logs" "$BASE_DIR/volumenes/mysql_log" "$BASE_DIR/volumenes/ftp_logs" "$BASE_DIR/volumenes/fakessh_logs" "$BASE_DIR/volumenes/mysql_data" "$BASE_DIR/volumenes/config" "$BASE_DIR/config"
 
 echo "Estructura inicial preparada."
